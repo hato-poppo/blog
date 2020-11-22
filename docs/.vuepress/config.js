4ab3990f261103166095f32eb53805696aa94b7b
hato-poppo <ysng1224@gmail.com>
2020-11-22 14:31:54 +0000

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: 'はとのす',
+
+  description: '技術ブログ',
+
+  themeConfig: {
+    lang: 'ja-JP',
+
+    nav: [
+      { text: 'Home', link: '/', exact: true },
+      { text: 'Posts', link: '/posts/', exact: false },
+    ],
+  }
+}
